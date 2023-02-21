3a587f6e994d8298b0c9008094647795b4b8835b
DeanRog14 <deanrog8844@gmail.com>
2023-02-21 14:53:57 -0600

    Javascript Tabs

---
+++ useage.js
@@ -0,0 +1,7 @@
+function Sch() {
+    window.open("https://www.maxpreps.com/tx/austin/bowie-bulldogs/soccer/winter/", "_blank");
+}
+
+function CtPg() {
+    window.open("contactus.html", "_blank");
+}
